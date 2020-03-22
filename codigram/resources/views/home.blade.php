7822e58042275c9b98a3438493f716445584897e
Abdulkader Freij <abdulkader@arch.localdomain>
2020-03-22 15:05:17 +0200

    User Auth

--- codigram/resources/views/home.blade.php
+++ codigram/resources/views/home.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="https://scontent.fbey5-1.fna.fbcdn.net/v/t1.0-0/p200x200/89966437_1410025839188497_220785040118775808_n.png?_nc_cat=102&_nc_sid=174925&_nc_ohc=nWKmNWtZdkkAX_5AZ-S&_nc_ht=scontent.fbey5-1.fna&oh=373c220456bbda7a4fa74c8a1facfc34&oe=5E9E6755" alt="profile-pic" class="rounded-circle"/>
         </div>
+        <div class="col-9 pt-5">
+        <div><h1>{{$user->username}}</h1></div>
+        <div class="d-flex">
+        <div class="pr-5"><strong>153</strong>posts</div>
+        <div class="pr-5"><strong>25k</strong>followers</div>
+        <div class="pr-5"><strong>212</strong>following</div>
+        </div>
+        <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+        <div>{{$user->profile->description}}</div>
+        <div><a href="https://codi.tech/" target="_blank">{{$user->profile->url}}</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+    <div class="col-4">
+    <img src="https://scontent.fbey5-2.fna.fbcdn.net/v/t1.0-0/p200x200/87028253_1399977326860015_2056681188889722880_o.jpg?_nc_cat=106&_nc_sid=a26aad&_nc_ohc=dAFqaUQUjJoAX_5xppO&_nc_ht=scontent.fbey5-2.fna&_nc_tp=6&oh=0d1f6171a0cfbd711acf1f0f4e710e43&oe=5E9C00DD" alt="" class="w-100"/>
+    </div>
+    <div class="col-4">
+    <img src="https://scontent.fbey5-1.fna.fbcdn.net/v/t1.0-0/p200x200/87498675_1399977043526710_6550896286948655104_o.jpg?_nc_cat=108&_nc_sid=a26aad&_nc_ohc=XgxNI16318UAX9y1QqU&_nc_ht=scontent.fbey5-1.fna&_nc_tp=6&oh=fb1df8c7397d62b18fe3f51e78f9d490&oe=5E9EA4DB" alt="" class="w-100"/>
+    </div>
+    <div class="col-4">
+    <img src="https://scontent.fbey5-2.fna.fbcdn.net/v/t1.0-0/p200x200/78359206_1315353088655773_1014283920251813888_o.jpg?_nc_cat=106&_nc_sid=9267fe&_nc_ohc=XLCSlpDmjCAAX85JTpR&_nc_ht=scontent.fbey5-2.fna&_nc_tp=6&oh=91a5761a40210a065c9da06a8408e362&oe=5E9B9A31" alt="" class="w-100"/>
+    </div>
     </div>
 </div>
 @endsection
--- codigram/resources/views/layouts/app.blade.php
+++ codigram/resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                <div><img src="https://scontent.fbey5-1.fna.fbcdn.net/v/t1.0-0/p200x200/89966437_1410025839188497_220785040118775808_n.png?_nc_cat=102&_nc_sid=174925&_nc_ohc=nWKmNWtZdkkAX_5AZ-S&_nc_ht=scontent.fbey5-1.fna&oh=373c220456bbda7a4fa74c8a1facfc34&oe=5E9E6755" style="height:20px; border-right:1px solid #333;" alt="" class="pr-3"/></div>
+                <div class="pl-3 pt-1">CodiGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
