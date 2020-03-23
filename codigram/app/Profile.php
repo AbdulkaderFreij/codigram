b83b3b2396fbce5edf6bb67588619ecca79364ea
Abdulkader Freij <abdulkader@arch.localdomain>
2020-03-23 14:42:59 +0200

    fix image

--- codigram/app/Profile.php
+++ codigram/app/Profile.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded=[];
-    
+
+    public function profileImage(){
+        $imagePath=($this->image) ?  $this->image : '/profile/0VIz7gS1rt6o1PCukIYnLcnoCKi7Swia1kkzZrI5.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
